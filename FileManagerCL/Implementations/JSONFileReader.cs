344244c7a15fba8e9b2c8c57a38b04a749602f8a
BilogMarwen <marwenkachroudi@gmail.com>
2018-04-15 11:13:19 +0200

    version 8

--- FileManagerCL/Implementations/JSONFileReader.cs
+++ FileManagerCL/Implementations/JSONFileReader.cs
@@ -1,4 +1,5 @@
-﻿using FileManagerCL.Interfaces;
+﻿using FileManagerCL.Helpers;
+using FileManagerCL.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,6 +15,27 @@ namespace FileManagerCL.Implementations
         public string ReadJSONFile(string filePath)
         {
             return base.ReadFile(filePath);
+        }
+        
+        // <summary>
+        /// <see cref="ICryptedFileReader.ReadCryptedFile(string filePath, CrypTingAlgorithm cryptingAlgorithm)"/> 
+        /// </summary> 
+        public override string ReadCryptedFile(string filePath, CrypTingAlgorithm cryptingAlgorithm)
+        {
+            string cryptedFileContent = this.ReadJSONFile(filePath);
+            string decrypedFileContent = String.Empty;
+
+            switch (cryptingAlgorithm)
+            {
+                case CrypTingAlgorithm.reverse:
+                    {
+                        decrypedFileContent = TextFileDecryptor.ReverseTextDecrypt(cryptedFileContent);
+                        break;
+                    }
+
+            }
+
+            return decrypedFileContent;
         }
     }
 }
